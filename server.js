de932e8e01082426b4cd1762edeb64bd14680d22
gstockha <54012873+gstockha@users.noreply.github.com>
2021-09-02 18:07:09 -0500

    1.0.9c working

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ console.log("Server running on port: " + port );
 const sport = 7102;
 const server = require('http').createServer();
 const io = require('socket.io')(server);
-io.set('origins', "http://127.0.0.1:51264/index.html")
+io.set('origins', "http://gotad.io:*")
 const version = "1.0.9";
 var total = 0; //total amount of clients that session
 var current = 0; //total amount of concurrent clients
