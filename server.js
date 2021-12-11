60dedeb63b9043b0c5f22beacf1c0c91f97064a4
gstockha <54012873+gstockha@users.noreply.github.com>
2021-12-11 11:47:22 -0600

    1.1.5 disable socket.io

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ console.log("Server running on port: " + port );
 //server time
 
 const sport = 7102;
-const server = require('http').createServer();
-const io = require('socket.io')(server);
-io.set('origins', "http://gotad.io:*")
-const version = "1.1.4";
+//const server = require('http').createServer();
+//const io = require('socket.io')(server);
+//io.set('origins', "http://gotad.io:*")
+const version = "1.1.5";
 var total = 0; //total amount of clients that session
 var current = 0; //total amount of concurrent clients
 var gameid = 0; //array index and total amount of games
@@ -81,36 +81,36 @@ module.exports = { //from client.js
 }
 
 // Listen for incoming connections
-server.listen(sport, (err) => {
-    if (err) throw err;
-    console.log("socket.io server running on port: " + sport );
-});
+// server.listen(sport, (err) => {
+//     if (err) throw err;
+//     console.log("socket.io server running on port: " + sport );
+// });
 
 
-io.on('connection', (client) => {
+// io.on('connection', (client) => {
 
-    total ++;
-    current ++;
-    let date = new Date();
-    console.log("browser player connected; " + current + " concurrent players connected, " + total + " players connected so far " + date.getHours() + ":" + date.getMinutes());
-    client.id = total;
+//     total ++;
+//     current ++;
+//     let date = new Date();
+//     console.log("browser player connected; " + current + " concurrent players connected, " + total + " players connected so far " + date.getHours() + ":" + date.getMinutes());
+//     client.id = total;
 
-    // Send clients SID
-    client.emit('server_id', {
-        server_id: version
-    });
-
-
-    //refresh gameslist
-    client.on('refresh', (data) => {
-        client.emit('games', gamechunk);
-    });
+//     // Send clients SID
+//     client.emit('server_id', {
+//         server_id: version
+//     });
 
 
-    client.on('disconnect', (data) => {
-        current --;
-        delete client;
-        //console.log("browser client " + client.id + " closed");
-    });
+//     //refresh gameslist
+//     client.on('refresh', (data) => {
+//         client.emit('games', gamechunk);
+//     });
 
-});
+
+//     client.on('disconnect', (data) => {
+//         current --;
+//         delete client;
+//         //console.log("browser client " + client.id + " closed");
+//     });
+
+// });
